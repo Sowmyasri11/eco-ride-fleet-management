91efce91cc40602ad2c54adbaf63447027312706
Sowmyasri11 <yenumulasowmya11@gmail.com>
2026-01-05 23:27:56 +0530

    adding welcome greeting

---
+++ src/ecofleet/models/main.py
@@ -0,0 +1,8 @@
+def greet():
+
+    #Displays a welcome message for the EcoRide Urban Mobility System
+    print("\n Welcome to Eco-ride Urban Mobility SYstem")
+
+if __name__ == '__main__':
+    greet()
+
